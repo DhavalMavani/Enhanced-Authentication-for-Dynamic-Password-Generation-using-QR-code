9ac9bfa639022714259592fe7e4171a47307dd3c
Dhaval Mavani <61201815+DhavalMavani@users.noreply.github.com>
2023-11-07 17:30:13 +0530

    c1

---
+++ qrscanner.py
@@ -0,0 +1,18 @@
+import cv2
+import webbrowser
+
+cap = cv2.VideoCapture(0)
+detector = cv2.QRCodeDetector()
+
+while True:
+    _,img=cap.read()
+    data,one, _=detector.detectAndDecode(img)
+    if data:
+        a=data
+        break
+    cv2.imshow('qrcodescanner app',img)    
+    if cv2.waitKey(1)==ord('q'):
+        break
+b=webbrowser.open(str(a)) 
+cap.release(a)
+cv2.destroyAllWindows()       
